4016e71718baf5f5355737acef5316bf4a7e4adc
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2022-12-17 17:35:05 +0200

    Add express and cors

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+app.use(cors())
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World!')
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
 });
